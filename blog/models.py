45d29723268fdffd856c08dfc436d0ee50d55346
Kofi <koficharlie@gmail.com>
2016-01-22 14:27:41 -0500

    Adding blog.models.get_absolute_url and created, slugfield to models

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 from media_explorer.fields import MediaField, RichTextField
 
 class Blog(models.Model):
@@ -6,6 +7,7 @@ class Blog(models.Model):
     Example Blog model
     """
 
+    slug = models.SlugField(unique=True, max_length=255)
     title = models.CharField(max_length=150)
 
     #If you do not provide a type then media can be image/video/gallery
@@ -14,9 +16,17 @@ class Blog(models.Model):
     #Providing a type will restrict the element to this type
     video = MediaField(type="video")
 
-    #Entry is changed from TextField to RichTextField
     #You will see a CKEditor WYSIWYG with DME plugin
     #NOTE: You cannot use more than one RichText field in a model
     entry = RichTextField()
 
-    published = models.BooleanField(default=False)
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args=[self.slug])
+
